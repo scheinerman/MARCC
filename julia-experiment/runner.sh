8efbbea6e88df83347aa7d9296b5930e80c87927
Edward Scheinerman <eschein1@jhu.edu@login-node03.cm.cluster>
2015-10-03 08:37:15 -0400

    Added julia experiment

---
+++ julia-experiment/runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+# run this script with "sbatch runner.sh"
+
+#SBATCH
+#SBATCH --job-name=JuliaExperiment
+#SBATCH --time=10
+#SBATCH --partition=parallel
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=24
+
+
+time julia -p 40 main.jl 50000000000 > harm.out
